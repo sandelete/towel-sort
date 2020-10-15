fd035ae8a252ae8f977f44c8f62a1e7d2657ee51
Саша Ряхов <rkhv@MacBook-Pro-Sasa.local>
2020-10-15 18:53:02 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (typeof(matrix) !== 'object') return [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    if ((i + 1) % 2 === 0) {
+      matrix[i].reverse();
+    }
+  }
+
+  return matrix.flat();
+};
